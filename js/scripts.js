94a1bdf3601a31ee5849bdad183c03ff22c1f2a0
Steve Burton and Suzi Rubino <suzi@>
2016-10-25 11:30:30 -0700

    add falsy statement to js

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,25 @@ $(document).ready(function() {
     var countBy = parseFloat($("input#countBy").val());
     var total = [];
 
+    if (!countTo || !countBy || countTo <= 0 || countBy === 0 || countTo === NaN || countBy === NaN ) {
+      // || countTo < countby ) {
+    // || !0 || countTo <= 0 || countBy <= 0) {
+      alert("Please enter a valid number");
+      $("form").removeClass(form-control);
+      $("form").addClass(form-control);
+
+    }
+
     for (var start = countBy; start <= countTo; start += countBy) {
 
       total.push(start);
     }
     $(".result").text(total);
 
+
+
     event.preventDefault();
+
   });
 
 });
